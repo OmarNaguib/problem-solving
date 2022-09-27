79a46d8192ce5310c96d80ad73ce1ea8ff779f1a
Omar Naguib <55961349+OmarNaguib@users.noreply.github.com>
2022-09-27 15:02:02 +0200

    Time: 66 ms, Memory: 13.9 MB - LeetHub

--- implement-strstr/implement-strstr.py
+++ implement-strstr/implement-strstr.py
@@ -1,7 +1,5 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        if len(needle)>len(haystack):
-            return -1
         for i in range(len(haystack)-len(needle)+1):
             result=True
             for j in range(len(needle)):
