1aced30d8a8019f5634f17107d6bc48a0261f439
Omar Naguib <55961349+OmarNaguib@users.noreply.github.com>
2024-04-12 22:54:02 +0200

    Time: 149 ms (88.24%), Space: 77.5 MB (52.94%) - LeetHub

---
+++ 2433-find-the-original-array-of-prefix-xor/2433-find-the-original-array-of-prefix-xor.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} pref
+ * @return {number[]}
+ */
+var findArray = function(pref) {
+    const result = []
+    result.push(pref[0])
+    for (let i = 1; i< pref.length; i++){
+        result.push(pref[i] ^ pref[i-1])
+    }
+    return result
+};
