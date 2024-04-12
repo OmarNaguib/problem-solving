14d53da96061799491c4e12df11570af6d553e76
Omar Naguib <55961349+OmarNaguib@users.noreply.github.com>
2024-04-12 03:02:14 +0200

    Time: 56 ms (63.35%), Space: 50.4 MB (68.31%) - LeetHub

--- 0067-add-binary/0067-add-binary.js
+++ 0067-add-binary/0067-add-binary.js
@@ -5,19 +5,19 @@
  */
 var addBinary = function(a, b) {
     let final=""
-    let remainder=0
+    let carry=0
     const max = Math.max(a.length, b.length)
     for (let i =1; i<=max;i++){
         const aBit = +(a[a.length - i]?? 0)
         const bBit = +(b[b.length - i]?? 0)
-        const result= aBit + bBit + remainder
+        const result= aBit + bBit + carry
         let bitResult;
-        if (result ===0) {bitResult="0";remainder=0}
-        if (result ===1) {bitResult="1";remainder=0}
-        if (result ===2) {bitResult="0";remainder=1}
-        if (result ===3) {bitResult="1";remainder=1}
+        if (result ===0) {bitResult="0";carry=0}
+        if (result ===1) {bitResult="1";carry=0}
+        if (result ===2) {bitResult="0";carry=1}
+        if (result ===3) {bitResult="1";carry=1}
         final =  bitResult + final
     }
-    if (remainder) final = remainder + final
+    if (carry) final = carry + final
     return final
 };
