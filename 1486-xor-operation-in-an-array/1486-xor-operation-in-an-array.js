5a2a5cd790bf3145a502cdbf4e4bd2ea93099aaa
Omar Naguib <55961349+OmarNaguib@users.noreply.github.com>
2024-04-12 23:34:56 +0200

    Time: 46 ms (84.10%), Space: 48.9 MB (41.03%) - LeetHub

---
+++ 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number} n
+ * @param {number} start
+ * @return {number}
+ */
+var xorOperation = function(n, start) {
+    let nums=[]
+    for (let i=0;i<n;i++) nums.push(start+2*i)
+    return nums.reduce((total,current)=>total^current)
+};
