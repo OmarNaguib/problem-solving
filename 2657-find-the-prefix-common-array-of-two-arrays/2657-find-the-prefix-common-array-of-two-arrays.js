d88e0a75aea38217acb22e1688d716a0b34034b7
Omar Naguib <55961349+OmarNaguib@users.noreply.github.com>
2024-04-14 01:30:22 +0200

    Time: 369 ms (5.34%), Space: 59.6 MB (15.27%) - LeetHub

---
+++ 2657-find-the-prefix-common-array-of-two-arrays/2657-find-the-prefix-common-array-of-two-arrays.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} A
+ * @param {number[]} B
+ * @return {number[]}
+ */
+var findThePrefixCommonArray = function(A, B) {
+//     loop through indexes
+//     add new elements to the set
+// push the length of the intersection between them
+    let result=[]
+    for (let i = 1;i<=A.length;i++){
+        const bSet= new Set([...B.slice(0,i)])
+        const intersection = A.slice(0,i).filter((number)=>bSet.has(number))
+        console.log(intersection)
+        result.push(intersection.length)
+    }
+    return result
+    
+};
