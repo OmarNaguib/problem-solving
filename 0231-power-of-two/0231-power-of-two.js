7eb0c0cb7766ac6c40fbc43dab3c0664907fbaca
Omar Naguib <55961349+OmarNaguib@users.noreply.github.com>
2024-05-01 03:50:30 +0300

    Time: 72 ms (74.07%), Space: 52.7 MB (82.91%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.js
+++ 0231-power-of-two/0231-power-of-two.js
@@ -3,6 +3,6 @@
  * @return {boolean}
  */
 var isPowerOfTwo = function(n) {
-    if (n<0) return 0
+    if (n<0) return false
     return n.toString(2).split("").filter(char=>char==="1").length === 1
 };
