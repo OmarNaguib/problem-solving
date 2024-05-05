f1b628b3d11c38ae08995ddfc1a4daf292561741
Omar Naguib <55961349+OmarNaguib@users.noreply.github.com>
2024-05-05 21:57:08 +0300

    Time: 64 ms (59.86%), Space: 52.3 MB (43.64%) - LeetHub

--- 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.js
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.js
@@ -10,12 +10,6 @@
  * @return {void} Do not return anything, modify node in-place instead.
  */
 var deleteNode = function(node) {
-    while (node.next.next){
-        node.val=node.next.val
-        node=node.next
-    }
     node.val = node.next.val
-    node.next=null
-
-    
+    node.next = node.next.next
 };
