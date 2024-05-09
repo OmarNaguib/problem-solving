ad7322d9acc6c97dddea9ef251e47b53db693e2f
Omar Naguib <55961349+OmarNaguib@users.noreply.github.com>
2024-05-09 15:42:38 +0300

    Time: 237 ms (77.85%), Space: 70.7 MB (13.47%) - LeetHub

--- 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.js
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.js
@@ -4,6 +4,6 @@
  * @return {number}
  */
 var maximumHappinessSum = function(happiness, k) {
-    return happiness.sort((a,b) => b-a ).slice(0,k)
-        .reduce((total,curr,i) => total + Math.max(0,curr-i),0) 
+    return happiness.sort((a, b) => b - a ).slice(0, k)
+        .reduce((total, curr, i) => total + Math.max(0, curr-i), 0) 
 };
