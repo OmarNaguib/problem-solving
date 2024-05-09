8246b160487f70b5bd781b6e84cc0443c667f8cf
Omar Naguib <55961349+OmarNaguib@users.noreply.github.com>
2024-05-09 23:59:01 +0300

    Time: 48 ms (92.67%), Space: 50 MB (26.47%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.js
+++ 0191-number-of-1-bits/0191-number-of-1-bits.js
@@ -3,19 +3,7 @@
  * @return {number}
  */
 var hammingWeight = function (n) {
-  let nBinary = "";
-  let counter = 1;
-  while (n > 0) {
-    const remainder = n % (2 ** counter);
-    if (remainder) {
-      nBinary = "1" + nBinary;
-      n -= 2 ** (counter - 1);
-    } else {
-      nBinary = "0" + nBinary;
-    }
-    counter++;
-  }
-  let count = 0;
-  for (char of nBinary) if (char === "1") count += 1;
-  return count;
+    return n.toString(2).split("").reduce((total,curr) => total + +(curr==="1"),0)
 };
+
+
