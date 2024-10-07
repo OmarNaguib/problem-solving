997c289327c7eda87afd42be70c63a6e70409d9e
Omar Naguib <55961349+OmarNaguib@users.noreply.github.com>
2024-10-08 02:55:45 +0300

    Time: 58 ms (31.31%), Space: 49.2 MB (74%) - LeetHub

--- 0920-uncommon-words-from-two-sentences/0920-uncommon-words-from-two-sentences.js
+++ 0920-uncommon-words-from-two-sentences/0920-uncommon-words-from-two-sentences.js
@@ -4,17 +4,12 @@
  * @return {string[]}
  */
 var uncommonFromSentences = function(s1, s2) {
-    const firstHash = {}
+    const wordCount = {}
     s1.split(" ").forEach((item)=>{
-        firstHash[item] = firstHash[item]? firstHash[item] + 1:1;
+        wordCount[item] = wordCount[item]? wordCount[item] + 1:1;
     })
-    const secondHash = {}
     s2.split(" ").forEach((item)=>{
-        secondHash[item] = secondHash[item]? secondHash[item] + 1:1;
+        wordCount[item] = wordCount[item]? wordCount[item] + 1:1;
     })
-    const firstUnique = new Set(Object.keys(firstHash).filter(key=>firstHash[key]===1))
-    s2.split(" ").forEach((item)=>firstUnique.delete(item))
-    const secondUnique = new Set(Object.keys(secondHash).filter(key=>secondHash[key]===1))
-    s1.split(" ").forEach((item)=>secondUnique.delete(item))
-    return [...firstUnique,...secondUnique]
+    return Object.keys(wordCount).filter(key=>wordCount[key]===1) 
 };
